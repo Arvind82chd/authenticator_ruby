023fde5cd4845dcd8504c78b8d64ca02d00ca933
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-04-09 13:29:13 -0700

    refactoring the code

--- index.rb
+++ index.rb
@@ -1,5 +1,5 @@
 # Fake Data:
-data = [
+users = [
     {username: "a", password: "pwd"},
     {username: "b", password: "pwd1"},
     {username: "c", password: "pwd2"},
@@ -7,17 +7,14 @@ data = [
     {username: "e", password: "111"}
 ]
 
-def authenticate(username, password, data)
-    data.each do |user|
-        if username == user[:username]
-            if password == user[:password]
-                puts "Welcome #{user[:username]} you have successfully loged in"
-                return user
-            else 
-                return "invalid username or password"
-            end
+def authenticate(username, password, users)
+    users.each do |user|
+        if username == user[:username] && password == user[:password]
+            puts "Welcome #{user[:username]} you have successfully loged in"
+            return user
         end
     end
+    "invalid username or password"
 end
 
 puts "Welcome to Authenticator app:"
@@ -30,4 +27,4 @@ puts "Enter your Username:"
 u = gets.chomp
 puts "Enter your password:"
 p = gets.chomp
-puts authenticate(u, p, data)
+puts authenticate(u, p, users)
