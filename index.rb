24555d33d9263b326dbeb9a0c6bd625941eea9ae
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-04-09 13:39:12 -0700

    adding no of attempts feature

--- index.rb
+++ index.rb
@@ -23,8 +23,16 @@ puts
 puts "This program will take input from the user and compare the password for authentication."
 puts "If password is correct, you will get back the user object."
 
-puts "Enter your Username:"
-u = gets.chomp
-puts "Enter your password:"
-p = gets.chomp
-puts authenticate(u, p, users)
+attepmts = 1
+while attepmts < 4
+    puts "Enter your Username:"
+    u = gets.chomp
+    puts "Enter your password:"
+    p = gets.chomp
+    puts authenticate(u, p, users)
+    puts "press n to break or enter to checkin as a different user"
+    input = gets.chomp.downcase
+    break if input == "n"
+    attepmts += 1
+end
+puts "You have exceeded the number of attempts"
